6f0ecaa6471eda64e5b1c6e3c1996566d7d8b44d
unknown <huzainzain5@gmail.com>
2022-08-25 23:16:58 +0530

    'Completed'

--- script.js
+++ script.js
@@ -1,2 +1,75 @@
-/* Your code goes here */
+function myFuncton(){
+
+    var x = document.getElementById("val1").value;
+    var a = 1;
+    var b = 1;
+    var res1 = (a/b);
+    if( res1 == x )
+    {
+    document.getElementById("thumb1").style.visibility = "visible";
+    document.getElementById("thumb2").style.visibility = "hidden" ;
+    }
+    else{
+        document.getElementById("thumb2").style.visibility = "visible";
+        document.getElementById("thumb1").style.visibility = "hidden" ;
+    }
+
+
+    var y = document.getElementById("val2").value;
+    var c = 13131444141;
+    var d = 1783687;
+    var res2 = (c/d);
+    if( res2 == y )
+    {
+    document.getElementById("thumb11").style.visibility = "visible";
+    document.getElementById("thumb22").style.visibility = "hidden" ;
+    }
+    else{
+        document.getElementById("thumb22").style.visibility = "visible";
+        document.getElementById("thumb11").style.visibility = "hidden" ;
+    }
+
+    var z = document.getElementById("val3").value;
+    var e = 145341563;
+    var f = 67563;
+    var res3 = (e-f);
+    if( res3 == z )
+    {
+    document.getElementById("thumb111").style.visibility = "visible";
+    document.getElementById("thumb222").style.visibility = "hidden" ;
+    }
+    else{
+        document.getElementById("thumb222").style.visibility = "visible";
+        document.getElementById("thumb111").style.visibility = "hidden" ;
+    }
+
+
+    var xx = document.getElementById("val4").value;
+    var aa = 1;
+    var bb = 1;
+    var res4 = (aa/bb);
+    if( res4 == xx )
+    {
+    document.getElementById("thumb1111").style.visibility = "visible";
+    document.getElementById("thumb2222").style.visibility = "hidden" ;
+    }
+    else{
+        document.getElementById("thumb2222").style.visibility = "visible";
+        document.getElementById("thumb1111").style.visibility = "hidden" ;
+    }
+
+    var xy = document.getElementById("val5").value;
+    var aaa = 1;
+    var bbb = 1;
+    var res5 = (aaa/bbb);
+    if( res5 == xy )
+    {
+    document.getElementById("thumb11111").style.visibility = "visible";
+    document.getElementById("thumb22222").style.visibility = "hidden" ;
+    }
+    else{
+        document.getElementById("thumb22222").style.visibility = "visible";
+        document.getElementById("thumb11111").style.visibility = "hidden" ;
+    }
+}
       
